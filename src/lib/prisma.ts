759823193375265f264602722c6a6830f64c72b6
Karan <mrkaran2k5@gmail.com>
2024-12-31 20:32:31 +0530

    Auth & DB sync

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+// From Documentation (prisma best practices)
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
