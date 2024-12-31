c38af9817bf62904999f54b0ec625693c4035449
Karan <mrkaran2k5@gmail.com>
2024-12-31 19:34:11 +0530

    Navbar and mobile sheet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import ModeToggle from "@/components/ToggleTheme";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        <SignInButton mode="modal"/>
-      </SignedOut>
-      <ModeToggle/>
+      Home
     </div>
   );
 }
